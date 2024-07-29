0cda269d698f865432e7a2c5cf7d13a767db83b6
ibado <ignaciobado.unc@gmail.com>
2024-07-29 20:46:46 +0200

    Implement day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -1,7 +1,24 @@
 use std::collections::HashMap;
 
 pub fn part1(input: &str) -> u32 {
-    let mut hm: HashMap<String, u32> = HashMap::new();
+    compute_dir_sizes(input)
+        .into_values()
+        .filter(|v| *v <= 100_000)
+        .sum()
+}
+
+pub fn part2(input: &str) -> u32 {
+    let sizes = compute_dir_sizes(input);
+    let needed_space = 30_000_000 - (70_000_000 - sizes.get("/").unwrap());
+    sizes
+        .into_values()
+        .filter(|v| *v >= needed_space)
+        .min()
+        .unwrap()
+}
+
+fn compute_dir_sizes(input: &str) -> HashMap<String, u32> {
+    let mut dir_sizes: HashMap<String, u32> = HashMap::new();
     let mut dirs: Vec<&str> = Vec::new();
     input
         .lines()
@@ -16,19 +33,15 @@ pub fn part1(input: &str) -> u32 {
             },
             d if d.as_bytes()[0].is_ascii_digit() => {
                 let size = d.split_once(" ").unwrap().0.parse::<u32>().unwrap();
-                let mut s = String::new();
+                let mut dir_key = String::new();
                 for dir in dirs.iter() {
-                    s.push_str(dir); // dirs can have subdirs with the same name
-                    *hm.entry(s.clone()).or_insert(0) += size;
+                    dir_key.push_str(dir); // dirs can have subdirs with the same name
+                    *dir_sizes.entry(dir_key.clone()).or_insert(0) += size;
                 }
             }
             _ => (),
         });
-    hm.into_values().filter(|v| *v <= 100_000).sum()
-}
-
-pub fn part2(_input: &str) -> u32 {
-    0
+    dir_sizes
 }
 
 #[cfg(test)]
@@ -36,7 +49,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn part1_sample_test2() {
+    fn part1_sample_test() {
         let input = "
             $ cd /
             $ ls
@@ -63,5 +76,35 @@ mod test {
             7214296 k";
 
         assert_eq!(part1(input), 95437);
+    }
+
+    #[test]
+    fn part2_sample_test() {
+        let input = "
+            $ cd /
+            $ ls
+            dir a
+            14848514 b.txt
+            8504156 c.dat
+            dir d
+            $ cd a
+            $ ls
+            dir e
+            29116 f
+            2557 g
+            62596 h.lst
+            $ cd e
+            $ ls
+            584 i
+            $ cd ..
+            $ cd ..
+            $ cd d
+            $ ls
+            4060174 j
+            8033020 d.log
+            5626152 d.ext
+            7214296 k";
+
+        assert_eq!(part2(input), 24933642);
     }
 }
