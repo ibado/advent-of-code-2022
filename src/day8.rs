230512ba20ad1e4da31b0c5ae53e7c0d7712089d
ibado <ignaciobado.unc@gmail.com>
2024-07-30 17:58:07 +0200

    Implement day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -1,16 +1,7 @@
-pub fn part1(input: &str) -> u32 {
-    let mx: Vec<_> = input
-        .lines()
-        .map(|l| {
-            l.trim()
-                .as_bytes()
-                .iter()
-                .map(|b| b - 48)
-                .collect::<Vec<_>>()
-        })
-        .filter(|l| !l.is_empty())
-        .collect();
+use std::cmp::max;
 
+pub fn part1(input: &str) -> u32 {
+    let mx = parse_matrix(input);
     let hight = mx.len();
     let width = mx[0].len();
 
@@ -31,8 +22,52 @@ pub fn part1(input: &str) -> u32 {
     count
 }
 
-pub fn part2(_input: &str) -> u32 {
-    0
+pub fn part2(input: &str) -> u32 {
+    let mx = parse_matrix(input);
+    let hight = mx.len();
+    let width = mx[0].len();
+
+    fn count_trees(iter: impl Iterator<Item = u8>, val: u8) -> u32 {
+        let mut count = 0;
+        for elem in iter {
+            if elem < val {
+                count += 1;
+            } else {
+                count += 1;
+                break;
+            }
+        }
+        count
+    }
+
+    let mut result: u32 = 0;
+    for i in 0..hight {
+        for j in 0..width {
+            let val = mx[i][j];
+            let up = count_trees((0..j).rev().map(|j| mx[i][j]), val);
+            let left = count_trees((0..i).rev().map(|i| mx[i][j]), val);
+            let right = count_trees(((i + 1)..hight).map(|i| mx[i][j]), val);
+            let down = count_trees(((j + 1)..width).map(|j| mx[i][j]), val);
+
+            result = max(result, up * left * right * down);
+        }
+    }
+
+    result
+}
+
+fn parse_matrix(input: &str) -> Vec<Vec<u8>> {
+    input
+        .lines()
+        .map(|l| {
+            l.trim()
+                .as_bytes()
+                .iter()
+                .map(|b| b - 48)
+                .collect::<Vec<_>>()
+        })
+        .filter(|l| !l.is_empty())
+        .collect()
 }
 
 #[cfg(test)]
@@ -49,5 +84,17 @@ mod test {
             35390";
 
         assert_eq!(part1(input), 21)
+    }
+
+    #[test]
+    fn part2_sample_input() {
+        let input = "
+            30373
+            25512
+            65332
+            33549
+            35390";
+
+        assert_eq!(part2(input), 8)
     }
 }
