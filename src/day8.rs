c9ae5d616502892bec808c2f1acdb5098a182d19
ibado <ignaciobado.unc@gmail.com>
2024-07-30 22:24:47 +0200

    Small refactor day 8 using closures

--- src/day8.rs
+++ src/day8.rs
@@ -1,63 +1,51 @@
 use std::cmp::max;
 
 pub fn part1(input: &str) -> u32 {
-    let mx = parse_matrix(input);
-    let hight = mx.len();
-    let width = mx[0].len();
+    let mut result = 0;
+    let n = input.trim().split_whitespace().count();
+    let is_in_edge = |i, j| i == 0 || i == n - 1 || j == 0 || j == n - 1;
+    parse_matrix(input, |mx, i, j| {
+        result += match mx[i][j] {
+            _ if is_in_edge(i, j) => 1,
+            val if val > (0..j).rev().map(|j| mx[i][j]).max().unwrap_or(9) => 1,
+            val if val > (0..i).rev().map(|i| mx[i][j]).max().unwrap_or(9) => 1,
+            val if val > ((i + 1)..n).map(|i| mx[i][j]).max().unwrap_or(9) => 1,
+            val if val > ((j + 1)..n).map(|j| mx[i][j]).max().unwrap_or(9) => 1,
+            _ => 0,
+        };
+    });
 
-    let mut count = 0;
-    let is_in_edge = |i, j| i == 0 || i == hight - 1 || j == 0 || j == width - 1;
-    for i in 0..hight {
-        for j in 0..width {
-            count += match mx[i][j] {
-                _ if is_in_edge(i, j) => 1,
-                val if val > (0..j).rev().map(|j| mx[i][j]).max().unwrap_or(9) => 1,
-                val if val > (0..i).rev().map(|i| mx[i][j]).max().unwrap_or(9) => 1,
-                val if val > ((i + 1)..hight).map(|i| mx[i][j]).max().unwrap_or(9) => 1,
-                val if val > ((j + 1)..width).map(|j| mx[i][j]).max().unwrap_or(9) => 1,
-                _ => 0,
-            };
-        }
-    }
-    count
+    result
 }
 
 pub fn part2(input: &str) -> u32 {
-    let mx = parse_matrix(input);
-    let hight = mx.len();
-    let width = mx[0].len();
-
+    let mut result = 0;
+    let n = input.trim().split_whitespace().count();
     fn count_trees(iter: impl Iterator<Item = u8>, val: u8) -> u32 {
         let mut count = 0;
         for elem in iter {
-            if elem < val {
-                count += 1;
-            } else {
-                count += 1;
+            count += 1;
+            if elem >= val {
                 break;
             }
         }
         count
     }
+    parse_matrix(input, |mx, i, j| {
+        let val = mx[i][j];
+        let up = count_trees((0..j).rev().map(|j| mx[i][j]), val);
+        let left = count_trees((0..i).rev().map(|i| mx[i][j]), val);
+        let right = count_trees(((i + 1)..n).map(|i| mx[i][j]), val);
+        let down = count_trees(((j + 1)..n).map(|j| mx[i][j]), val);
 
-    let mut result: u32 = 0;
-    for i in 0..hight {
-        for j in 0..width {
-            let val = mx[i][j];
-            let up = count_trees((0..j).rev().map(|j| mx[i][j]), val);
-            let left = count_trees((0..i).rev().map(|i| mx[i][j]), val);
-            let right = count_trees(((i + 1)..hight).map(|i| mx[i][j]), val);
-            let down = count_trees(((j + 1)..width).map(|j| mx[i][j]), val);
-
-            result = max(result, up * left * right * down);
-        }
-    }
+        result = max(result, up * left * right * down);
+    });
 
     result
 }
 
-fn parse_matrix(input: &str) -> Vec<Vec<u8>> {
-    input
+fn parse_matrix(input: &str, mut block: impl FnMut(&Vec<Vec<u8>>, usize, usize) -> ()) {
+    let mx: Vec<Vec<u8>> = input
         .lines()
         .map(|l| {
             l.trim()
@@ -67,7 +55,14 @@ fn parse_matrix(input: &str) -> Vec<Vec<u8>> {
                 .collect::<Vec<_>>()
         })
         .filter(|l| !l.is_empty())
-        .collect()
+        .collect();
+
+    let n = mx.len();
+    for i in 0..n {
+        for j in 0..n {
+            block(&mx, i, j);
+        }
+    }
 }
 
 #[cfg(test)]
